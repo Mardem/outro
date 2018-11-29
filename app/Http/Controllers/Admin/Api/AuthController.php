2220fed4b162526778f19948cca63a016f2a6e27
M. Cavalcante <mardenmc22@gmail.com>
2018-11-29 12:00:33 -0200

    Iniciado - Proteção de API com JWT

--- app/Http/Controllers/Admin/Api/AuthController.php
+++ app/Http/Controllers/Admin/Api/AuthController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Admin\Api;
 
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+    
+    public function login()
+    {
+        
+    }
 }
