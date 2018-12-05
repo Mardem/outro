eeb0c18eb55bec073e307f59f437cf3687c2ca2e
Davi Lopes Fernandes Junior <davi_juniordf@hotmail.com>
2018-12-05 15:26:09 -0200

    ajuste tipo do socio

--- app/Http/Controllers/Admin/SociosController.php
+++ app/Http/Controllers/Admin/SociosController.php
@@ -18,7 +18,8 @@ class SociosController extends Controller
     public function index()
     {
         \Auth::user()->category == 1 ? $s = Socio::orderBy('id', 'desc')->paginate() : $s = Socio::orderBy('id', 'desc')->where('operador_id', \Auth::user()->category)->paginate();
-        return view('admin.controle.socios.index')->with(['socios' => $s]);
+        $total = Socio::all();
+        return view('admin.controle.socios.index')->with(['socios' => $s, 'todos' => $total]);
     }
 
     /**
--- resources/views/admin/controle/socios/view.blade.php
+++ resources/views/admin/controle/socios/view.blade.php
@@ -38,6 +38,20 @@
                         </div>
 
                         <div class="form-group">
+                            <label for="tipo">Tipo*:</label>
+                            <select name="tipo" class="form-control" id="tipo">
+                                <option value="Remidos">Remidos</option>
+                                <option value="Gold">Gold</option>
+                                <option value="Plus">Plus</option>
+                                <option value="Vip">Vip</option>
+                                <option value="Vivendas">Vivendas</option>
+                                <option value="Contribuintes">Contribuintes</option>
+                                <option value="Titulos Sênior">Titulos Sênior</option>
+                                <option value="Thermas">Thermas</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
                             <label for="name">Nome*:</label>
                             <input type="text" class="form-control" id="name" name="nome"
                                    placeholder=" Digite o nome do sócio" value="{{ $socio->nome }}" required>
@@ -145,7 +159,7 @@
         </div>
 
         <div class="row">
-            <div class="col-sm-12 grid-margin strech-car">
+            <div class="col-sm-12 grid-margin strech-card">
                 <div class="card">
                     <div class="card-body">
                         <button class="btn btn-block btn-outline-success" type="submit" id="salvar">Atualizar dados</button>
@@ -155,6 +169,101 @@
         </div>
 
     </form>
+    <div class="row">
+        <div class="col-sm-12 grid-margin strech-card">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Outros dados</h4>
+
+                    <div class="row clearfix">
+                        <div class="col-md-3">
+                            <label for="data_nascimento">Data Nascimento:</label>
+                            <input type="text" class="form-control pula" id="data_nascimento"
+                                   value="{{ $socio->data_nascimento }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="type">Tipo*:</label>
+                            <input type="text" class="form-control" id="type" value="{{ $socio->type }}" disabled>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="fone1">Fone 1:</label>
+                            <input type="text" class="form-control pula" id="fone1" value="{{ $socio->fone1 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone2">Fone 2:</label>
+                            <input type="text" class="form-control pula" id="fone2" value="{{ $socio->fone2 }}" disabled>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="col-md-3">
+                            <label for="fone3">Fone 3:</label>
+                            <input type="text" class="form-control pula" id="fone3" value="{{ $socio->fone3 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone4">Fone 4:</label>
+                            <input type="text" class="form-control" id="fone4" value="{{ $socio->fone4 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone5">Fone 5:</label>
+                            <input type="text" class="form-control" id="fone5" value="{{ $socio->fone5 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone6">Fone 6:</label>
+                            <input type="text" class="form-control" id="fone6" value="{{ $socio->fone6 }}" disabled>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="col-md-3">
+                            <label for="fone7">Fone 7:</label>
+                            <input type="text" class="form-control" id="fone7" value="{{ $socio->fone7 }}" disabled>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="fone8">Fone 8:</label>
+                            <input type="text" class="form-control" id="fone8" value="{{ $socio->fone8 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone9">Fone 9:</label>
+                            <input type="text" class="form-control" id="fone9" value="{{ $socio->fone9 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone10">Fone 10:</label>
+                            <input type="text" class="form-control" id="fone10" value="{{ $socio->fone10 }}" disabled>
+                        </div>
+                    </div>
+
+                    <div class="row clearfix">
+                        <div class="col-md-3">
+                            <label for="fone11">Fone 11:</label>
+                            <input type="text" class="form-control" id="fone11" value="{{ $socio->fone11 }}" disabled>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="fone12">Fone 12:</label>
+                            <input type="text" class="form-control" id="fone12" value="{{ $socio->fone12 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone13">Fone 13:</label>
+                            <input type="text" class="form-control" id="fone13" value="{{ $socio->fone13 }}" disabled>
+                        </div>
+
+                        <div class="col-md-3">
+                            <label for="fone14">Fone 14:</label>
+                            <input type="text" class="form-control" id="fone14" value="{{ $socio->fone14 }}" disabled>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 @endsection
